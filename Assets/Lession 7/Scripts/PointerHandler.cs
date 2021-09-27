fa270e74bf96e3e30935f72fd40e2db73d321518
haivd <haivd@zitga.com.vn>
2021-09-27 08:45:30 +0700

    EventSystem

--- Assets/Lession 7/Scripts/PointerHandler.cs
+++ Assets/Lession 7/Scripts/PointerHandler.cs
@@ -1,39 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PointerHandler : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerExitHandler, IPointerUpHandler
+public class PointerHandler : EventTrigger
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnMouseEnter()
     {
-        
+        Debug.Log("Mouse Enter");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnPointerEnter(PointerEventData eventData)
     {
-        
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("Cursor Entering " + name + " GameObject");
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("Cursor Down " + name + " GameObject");
+        Debug.Log("OnPointerEnter Enter");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("Cursor Exit " + name + " GameObject");
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        Debug.Log("Cursor Up " + name + " GameObject");
+        Debug.Log("OnPointerExit Enter");
     }
 }
